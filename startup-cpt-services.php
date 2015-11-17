23f6eff6cef2299a22efd382656dfe47c76b02b8
yozz <yozz.online@gmail.com>
2015-11-17 15:22:54 -0500

    Internationalization

--- startup-cpt-services.php
+++ startup-cpt-services.php
@@ -34,25 +34,25 @@ add_action( 'init', 'startup_reloaded_services_updater' );
 //CPT
 function startup_reloaded_services() {
 	$labels = array(
-		'name'                => _x( 'Services', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Service', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Services', 'text_domain' ),
-		'name_admin_bar'      => __( 'Services', 'text_domain' ),
-		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-		'all_items'           => __( 'All Items', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
-		'add_new'             => __( 'Add New', 'text_domain' ),
-		'new_item'            => __( 'New Item', 'text_domain' ),
-		'edit_item'           => __( 'Edit Item', 'text_domain' ),
-		'update_item'         => __( 'Update Item', 'text_domain' ),
-		'view_item'           => __( 'View Item', 'text_domain' ),
-		'search_items'        => __( 'Search Item', 'text_domain' ),
-		'not_found'           => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' )
+		'name'                => _x( 'Services', 'Post Type General Name', 'startup-cpt-services' ),
+		'singular_name'       => _x( 'Service', 'Post Type Singular Name', 'startup-cpt-services' ),
+		'menu_name'           => __( 'Services', 'startup-cpt-services' ),
+		'name_admin_bar'      => __( 'Services', 'startup-cpt-services' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'startup-cpt-services' ),
+		'all_items'           => __( 'All Items', 'startup-cpt-services' ),
+		'add_new_item'        => __( 'Add New Item', 'startup-cpt-services' ),
+		'add_new'             => __( 'Add New', 'startup-cpt-services' ),
+		'new_item'            => __( 'New Item', 'startup-cpt-services' ),
+		'edit_item'           => __( 'Edit Item', 'startup-cpt-services' ),
+		'update_item'         => __( 'Update Item', 'startup-cpt-services' ),
+		'view_item'           => __( 'View Item', 'startup-cpt-services' ),
+		'search_items'        => __( 'Search Item', 'startup-cpt-services' ),
+		'not_found'           => __( 'Not found', 'startup-cpt-services' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'startup-cpt-services' )
 	);
 	$args = array(
-		'label'               => __( 'services', 'text_domain' ),
-		'description'         => __( '', 'text_domain' ),
+		'label'               => __( 'services', 'startup-cpt-services' ),
+		'description'         => __( '', 'startup-cpt-services' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'revisions' ),
 		'hierarchical'        => true,
@@ -113,13 +113,13 @@ function startup_reloaded_services_meta() {
 
 	$cmb_box = new_cmb2_box( array(
 		'id'            => $prefix . 'metabox',
-		'title'         => __( 'Service details', 'cmb2' ),
+		'title'         => __( 'Service details', 'startup-cpt-services' ),
 		'object_types'  => array( 'services' )
 	) );
     
     $cmb_box->add_field( array(
             'name'             => __( 'Icon', 'cmb2' ),
-            'desc'             => __( 'The service icon', 'cmb2' ),
+            'desc'             => __( 'The service icon', 'startup-cpt-services' ),
             'id'               => $prefix . 'icon',
             'type'             => 'select',
             'show_option_none' => true,
