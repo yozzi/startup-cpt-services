448d82290b0663f8641e39185204793af339aeb2
yozz <yozz.online@gmail.com>
2016-01-13 16:40:37 -0500

    Format

--- startup-cpt-services.php
+++ startup-cpt-services.php
@@ -166,7 +166,7 @@ if ( !function_exists( 'shortcode_ui_detection' ) ) {
         }
     }
 
-add_action( 'init', 'shortcode_ui_detection' );
+    add_action( 'init', 'shortcode_ui_detection' );
 }
 
 function startup_cpt_services_shortcode_ui() {
